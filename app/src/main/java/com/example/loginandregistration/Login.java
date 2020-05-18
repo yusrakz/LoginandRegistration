545198e913471ba26acf054106f0265ce87ff626
yusrakz <64756540+yusrakz@users.noreply.github.com>
2020-05-18 06:30:42 +0500

    Initial commit

---
+++ app/src/main/java/com/example/loginandregistration/Login.java
@@ -0,0 +1,39 @@
+package com.example.loginandregistration;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Login extends AppCompatActivity {
+
+    EditText e1, e2;
+    Button b1;
+    DatabaseHelper db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        db = new DatabaseHelper(this);
+        e1 = (EditText) findViewById(R.id.editText);
+        e2 = (EditText) findViewById(R.id.editText2);
+        b1 = (Button) findViewById(R.id.button);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = e1.getText().toString();
+                String password = e2.getText().toString();
+                Boolean Chkemailpass = db.emailpassword(email,password);
+                if(Chkemailpass==true)
+                    Toast.makeText(getApplicationContext(),"Successfully Login", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(getApplicationContext(),"Wrong email or password", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/loginandregistration/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.loginandregistration;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    DatabaseHelper db;
+    Button b1, b2;
+    EditText e1, e2, e3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        db = new DatabaseHelper(this);
+        b1 = (Button)findViewById(R.id.register);
+        b2 = (Button) findViewById(R.id.button2);
+
+        e1 = (EditText)findViewById(R.id.email);
+        e2 = (EditText)findViewById(R.id.pass);
+        e3 = (EditText) findViewById(R.id.cpass);
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, Login.class);
+                startActivity(i);            }
+        });
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String s1 = e1.getText().toString();
+                String s2 = e2.getText().toString();
+                String s3 = e3.getText().toString();
+                if (s1.equals("")|| s2.equals("")||s3.equals("")){
+                    Toast.makeText(getApplicationContext(),"Fields are empty",Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    if (s2.equals(s3)) {
+                        Boolean chkemail = db.chkemail(s1);
+                        if (chkemail == true) {
+                            Boolean insert = db.insert(s1, s2);
+                            if (insert == true) {
+                                Toast.makeText(getApplicationContext(), "Registered Successfully", Toast.LENGTH_SHORT).show();
+                            }
+                        } else {
+                            Toast.makeText(getApplicationContext(), "Email Already exists", Toast.LENGTH_SHORT).show();
+
+                        }
+                        Toast.makeText(getApplicationContext(),"Password donot match", Toast.LENGTH_SHORT).show();
+
+                    }
+                }
+            }
+        });
+
+    }
+}
